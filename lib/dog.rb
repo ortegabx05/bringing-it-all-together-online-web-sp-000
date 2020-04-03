852e9a727fcc5278ed114a3a04fcb12de8feaeee
jessi ortega <ortegabx05@gmail.com>
2020-04-03 03:19:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,4 +52,22 @@ class Dog
         new_dog
    end
 
+   def self.find_or_create_by(name:name,breed:breed)
+      sql = <<-SQL
+            SELECT *
+            FROM dogs
+            WHERE name = ?
+            AND breed = ?
+            LIMIT 1
+             SQL
+      dog = DB[:conn].execute(sql,name,breed)
+        if !dog.empty?
+            dog = dog[0]
+            new_dog = Dog.new(id:dog[0],name:dog[1],breed:dog[2])
+        else
+            new_dog = self.create(name:name,breed:breed)
+        end
+      new_dog
+   end
+    
   end
